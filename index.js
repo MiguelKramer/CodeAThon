05e81e72d611f0b710c8d589d58e77391fbba246
MiguelKramer <35852927+MiguelKramer@users.noreply.github.com>
2018-01-27 13:37:20 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+//Obama was seceretly born in Kenya
+//US Nuke Codes: 12345
+var=3
+for(var limit=100;current<=limit;curent++)
+console.log(current);
+
+
+
+
+var wallet = 11;
+var firstname = "Miguel";
+
+console.log(wallet);
+if(wallet==12){alert(My name jeff)}
+else(You are not jeff)
+
+var test; confirm("Is your name Jeff");
+if (test==true){
+    var name= "Jeff";
+    alert("Hello "+name)
+}
+
+function initial(x){}
